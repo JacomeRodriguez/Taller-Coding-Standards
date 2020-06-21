16aa1d15975d3726a75ec6af51ebdd891903cfab
JacomeRodriguez <willjey.jacome7@gmail.com>
2020-06-21 05:22:25 -0500

    Code Without error

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -1,78 +1,89 @@
-//
-//
-
+// Copyright (C) 2020
+// All rights reserved
+	
 package labcodingstandards;
 
 import java.util.Scanner;
 
 
 /**
- * @author
+ * @author William Jácome
  *
  */
-public class Calculator {	
-//CHECKSTYLE: OFF
-	public static void main(String[] args) {
-//CHECKSTYLE: ON
+
+public class Calculator {
+	/** Método principal
+	 *  @param args argumentos a pasar
+	 */
+	//CHECKSTYLE: OFF
+	public static void main (String[] args) {
+	//CHECKSTYLE: ON
 		Scanner reader = new Scanner(System.in);
-		
+
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
-        
+
         char operator = reader.nextLine().charAt(0);
-        double First;
+        double first;
         double second;
         String input;
-        
+
         while (true) {
         	System.out.print("Enter first number: ");
         	input = reader.nextLine();
-        	
+
             try {
-            	First=Integer.parseInt(input);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Not valid!");
-            }
-        }
-        
-        while (true) {
-        	System.out.print("Enter second number: ");
-        	input = reader.nextLine();
-        	
-            try {
-            	second=Integer.parseInt(input);
+            	first = Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
             }
         }
 
-        Calculator cal=new Calculator();
-        String result=cal.Operation(First,second,operator);
+        while (true) {
+        	System.out.print("Enter second number: ");
+        	input = reader.nextLine();
+
+            try {
+            	second = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Not valid!");
+            }
+        }
+
+        Calculator cal = new Calculator();
+        String result = cal.operation(first, second, operator);
 
         System.out.printf(result);
 		reader.close();
 	}
 
-	private String Operation(double first,double second,char operator) {
+	/**
+	 * Este metodo realiza una operación aritmetica y retorna el valor.
+	 * @param first double  num1
+	 * @param second double num2
+	 * @param operator char operador-aritmentico
+	 * @return result double resultado
+	 */
+
+	private String operation(double first, double second, char operator) {
 		double result = 0;
-		switch(operator)
-        {
+		switch (operator) {
             case '1':
-                result=first+second;
+                result = first + second;
                 break;
             case '2':
-                result=first-second;
+                result = first - second;
                 break;
             case '3':
-                result=first*second;
+                result = first * second;
                 break;
             case '4':
-                result =first/second;
+                result = first / second;
                 break;
             default:
             	return "Error! operator is not correct";
         }
-		return "The result is: "+result;
+		return "The result is: " + result;
 	}
 }
